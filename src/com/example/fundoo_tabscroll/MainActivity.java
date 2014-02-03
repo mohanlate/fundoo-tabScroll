e32e61c8ddffd8ea5cb6d357740bea19bd79a233
mohanlate <mohan.late@gmail.com>
2014-02-04 02:30:07 +0530

    All 8 screens added to scroll tab

--- src/com/example/fundoo_tabscroll/MainActivity.java
+++ src/com/example/fundoo_tabscroll/MainActivity.java
@@ -59,13 +59,30 @@ class MyAdapter extends FragmentStatePagerAdapter
         if(i==3)
         {
             fragment=new Messages();
-        }        return fragment;
+        }        
+        if(i==4)
+        {
+            fragment=new Like();
+        }        
+        if(i==5)
+        {
+            fragment=new Gallery();
+        }        
+        if(i==6)
+        {
+            fragment=new Tellafriend();
+        }        
+        if(i==7)
+        {
+            fragment=new ContactUs();
+        }        
+        return fragment;
     }
 
     @Override
     public int getCount() {
 //        Log.d("VIVZ", "get Count is called");
-        return 4;
+        return 8;
     }
 
     @Override
@@ -85,6 +102,22 @@ class MyAdapter extends FragmentStatePagerAdapter
         if(position==3)
         {
             return "Messages";
+        }
+        if(position==4)
+        {
+            return "Like Us";
+        }
+        if(position==5)
+        {
+            return "Gallery";
+        }
+        if(position==6)
+        {
+            return "Tell a Friend";
+        }
+        if(position==7)
+        {
+            return "Contact Us";
         }
         return null;
     }
